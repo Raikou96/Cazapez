cc165a543d1b85db466c0e09e452c19e3f6490f2
Jose <josecmr96@gmail.com>
2025-06-03 23:51:08 +0200

    Primera versión

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function App() {
+  return (
+    <div className="p-4 text-center text-xl text-green-800">
+      ¡Bienvenido a CazaPez!
+    </div>
+  )
+}
+
+export default App
